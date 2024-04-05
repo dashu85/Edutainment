9d0d2b3bfdb9212da692e6341fd28382e79fc9ad
Marcus Benoit <benoit.marcus@gmail.com>
2024-04-05 13:05:33 +0200

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Marcus Benoit on 05.04.24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
