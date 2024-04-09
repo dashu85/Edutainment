e376665bc61d43d519cfdef17a3407965b0a08eb
Marcus Benoit <benoit.marcus@gmail.com>
2024-04-09 15:07:30 +0200

    Start page is set up, multiplicator, number of questions and start button are working

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -7,15 +7,33 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct StartScreen: View {
+    
+    @State private var multiplicator: Int
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                RadialGradient(stops: [
+                    .init(color: Color(red: 0.992, green: 0.882, blue: 0), location: 0.3),
+                    .init(color: Color(red: 0, green: 0, blue: 0), location: 0.3),
+                ], center: .top, startRadius: 200, endRadius: 1100)
+                .ignoresSafeArea()
+                
+                VStack {
+                    Form {
+                        Section {
+                            Stepper("Let's play \(multiplicator)", value: $multiplicator, in: 1...12)
+                        } header: {
+                            Text("Which number do you want to play?")
+                        }
+                        
+                        
+                    }
+                    .navigationTitle("Edutainment")
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ Edutainment/Questions.swift
@@ -0,0 +1,8 @@
+//
+//  Questions.swift
+//  Edutainment
+//
+//  Created by Marcus Benoit on 08.04.24.
+//
+
+import Foundation
